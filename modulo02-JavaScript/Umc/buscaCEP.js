4dc503990456fa3cd2df01015d839a3f47ea4883
Matheus Luiz M. Silva <131410397+Kei2077@users.noreply.github.com>
2025-10-03 10:36:09 -0300

    Improve CEP lookup error handling and field population
    Added error message display for invalid CEPs and refactored code to clear address fields on error. Introduced a helper function to populate address fields from the API response. Updated the CEP input maxlength in the HTML to 10 for better flexibility.

--- modulo02-JavaScript/Umc/buscaCEP.js
+++ modulo02-JavaScript/Umc/buscaCEP.js
@@ -7,12 +7,42 @@ cep.addEventListener("change", (evento) => {
 })
 
 async function buscaCEP(cepUsuario){
+    
+    let erroCep = document.getElementById("erro")
+    erroCep.innerHTML = ""
     try{
         let consultaCEP = await fetch(`http://viacep.com.br/ws/${cepUsuario}/json`)
         let consultaCEPJson = await consultaCEP.json()
-        
+        preencheCampos(consultaCEPJson)
+
+        if (consultaCEPJson.erro){
+            throw Error ("CEP inexistente")
+        }
     }
     catch{
+        erroCep.innerHTML = "CEP INVALIDO"
+        rua.value = ""
+        bairro.value = ""
+        cidade.value = ""
+        estado.value = ""
+
 
     }
+}
+
+function preencheCampos(cepJson){
+    console.log(cepJson);
+    console.log(cepJson.logradouro);
+
+    let rua = document.getElementById("rua")
+    let cidade = document.getElementById("cidade")
+    let estado = document.getElementById("estado")
+    let bairro = document.getElementById("bairro")
+
+    rua.value = cepJson.logradouro
+    bairro.value = cepJson.bairro
+    cidade.value = cepJson.localidade
+    estado.value = cepJson.uf
+
+
 }
